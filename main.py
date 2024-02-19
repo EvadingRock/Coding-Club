aa9fb20223f09139f4e42c07b77481bf48e5fa4b
Caden Zhang <cadenz.@Cadens-MacBook-Air.local>
2024-02-18 19:05:39 -0500

    testing commit

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ from Using import Q1_Secret_Santa
 # import Spare_Storage
 
 
-# from random import randint
-#
-# case = list(range(1, 31))
-#
-# all_case = []
-#
-# for x in case:
-#     to_add = case[:]
-#
-#     to_add.pop(x - 1)
-#
-#     rand_amount = randint(0, 28)
-#     for y in range(rand_amount):
-#         to_add.pop(randint(0, len(to_add) - 1))
-#
-#     to_add.insert(0, 0)
-#     all_case.append(to_add)
-#
-# print(30)
-# for x in all_case:
-#     print(" ".join([str(i) for i in x]))
+from random import randint
+
+case = list(range(1, 31))
+
+all_case = []
+
+for x in case:
+    to_add = case[:]
+
+    to_add.pop(x - 1)
+
+    rand_amount = randint(0, 28)
+    for y in range(rand_amount):
+        to_add.pop(randint(0, len(to_add) - 1))
+
+    to_add.insert(0, 0)
+    all_case.append(to_add)
+
+print(30)
+for x in all_case:
+    print(" ".join([str(i) for i in x]))
